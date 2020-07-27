c5f5f422764616b72642becc707b99b0f9146953
aprilfaithdays <aprilfaith.e@gmail.com>
2020-07-27 00:08:50 -0500

    updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Footer = () =>{
     return(
-        <small><em>
+        <small>
             2020 StyleMe | {' '}
-            <a href="https://www.linkedin.com/in/april-escobar/" target="_blank" rel="noopener noreferrer">April Escobar</a> | {' '}
-            <a href="https://github.com/aprilfaithdays" target="_blank" rel="noopener noreferrer">Github</a>
-        </em></small>
+            <a href="https://www.aprilescobar.com/" target="_blank" rel="noopener noreferrer">April Escobar</a>
+        </small>
 
     )
 }
